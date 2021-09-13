27d9576a772767a86072fd753d65d0cb496e1837
vahe-git <hovhannisyanvahe17@gmail.com>
2021-09-13 15:55:18 +0400

    winner

--- utils/cache.js
+++ utils/cache.js
@@ -3,7 +3,7 @@ const myCache = new NodeCache( { stdTTL: 100, checkperiod: 120 } );
 const set = (key,data)=>{
     return myCache.set( key, data, 10000 );
 }
-const get = (key,)=>{
+const get = (key)=>{
    return  myCache.get( key );
 }
 module.exports = {
